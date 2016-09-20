e5d3f1487fbd1600c261e71cfc74a780f78d029f
thesuss <lullisuss@gmail.com>
2016-09-20 14:28:12 +0200

    User table has all relevant db columns

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,32 @@ RSpec.describe User, type: :model do
   it 'should have valid Factory' do
     expect(FactoryGirl.create(:user)).to be_valid
   end
+
+  describe 'Database table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :provider }
+    it { is_expected.to have_db_column :uid }
+    it { is_expected.to have_db_column :encrypted_password }
+    it { is_expected.to have_db_column :reset_password_token }
+    it { is_expected.to have_db_column :reset_password_sent_at }
+    it { is_expected.to have_db_column :remember_created_at }
+    it { is_expected.to have_db_column :sign_in_count }
+    it { is_expected.to have_db_column :current_sign_in_at }
+    it { is_expected.to have_db_column :last_sign_in_at }
+    it { is_expected.to have_db_column :current_sign_in_ip }
+    it { is_expected.to have_db_column :last_sign_in_ip }
+    it { is_expected.to have_db_column :confirmation_token }
+    it { is_expected.to have_db_column :confirmed_at }
+    it { is_expected.to have_db_column :confirmation_sent_at }
+    it { is_expected.to have_db_column :unconfirmed_email }
+    it { is_expected.to have_db_column :nickname }
+    it { is_expected.to have_db_column :image }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :tokens }
+    it { is_expected.to have_db_column :created_at }
+    it { is_expected.to have_db_column :updated_at }
+  end
+
+
+  
 end
