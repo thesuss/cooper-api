52f72aaba69318793fbe713447cce97796e60309
thesuss <lullisuss@gmail.com>
2016-09-20 14:58:23 +0200

    added performance data test and controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,7 +29,10 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column :created_at }
     it { is_expected.to have_db_column :updated_at }
   end
-
+  
+  describe 'Relations' do
+    it { is_expected.to have_many :performance_data }
+  end
 
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:email) }
