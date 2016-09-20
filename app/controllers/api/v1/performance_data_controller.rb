af99459ee9f8b4856813d96aae95d83c8e52824a
thesuss <lullisuss@gmail.com>
2016-09-20 14:59:58 +0200

    added create to controller

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,3 +1,8 @@
 class Api::V1::PerformanceDataController < ApplicationController
-
+  def create
+    @data = PerformanceData.new(params[:performance_data])
+    if @data.save
+      render json: { message: 'all good' }
+    end
+  end
 end
