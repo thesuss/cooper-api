8b054522948d6fc07e8e8aa6783ef365f2547017
thesuss <lullisuss@gmail.com>
2016-09-20 15:04:34 +0200

    added user to performance data api test

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::PerformanceDataController < ApplicationController
+  before_action :authenticate_api_v1_user!
+
   def create
-    @data = PerformanceData.new(performance_data_params)
+    @data = PerformanceData.new(performance_data_params.merge(user: current_api_v1_user))
 
     if @data.save
       render json: { message: 'all good' }
@@ -12,6 +14,7 @@ class Api::V1::PerformanceDataController < ApplicationController
   private
 
   def performance_data_params
+    #Figure out a better way
     params.require(:performance_data).permit!
   end
 end
