fb18fdb5b0415c50f66159c9e76c703d3c9708ca
thesuss <lullisuss@gmail.com>
2016-09-20 15:06:54 +0200

    performance data gets registrered in tests

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -1,12 +1,13 @@
 RSpec.describe Api::V1::PerformanceDataController, type: :request do
-  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
 
   describe 'POST /api/v1/performance_data' do
     it 'creates a data entry' do
-      post '/api/v1/performance_data', params: {
-        performance_data: { data: { message: 'Average' } }
-      }, headers: headers
-
+      post '/api/v1/performance_data',
+        params: { performance_data: { data: { message: 'Average' } } },
+        headers: headers
       entry = PerformanceData.last
       expect(entry.data).to eq 'message' => 'Average'
     end
